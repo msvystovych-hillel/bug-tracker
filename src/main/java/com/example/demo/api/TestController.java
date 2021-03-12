46ce5274b9b09fbdbe4668cbf579c2fcbc84759d
Maksym Svystovych <none>
2021-03-12 20:52:47 +0200

    add connection pool

--- src/main/java/com/example/demo/api/TestController.java
+++ src/main/java/com/example/demo/api/TestController.java
@@ -1,6 +1,6 @@
 package com.example.demo.api;
 
-import com.example.demo.repository.TestRepository;
+import com.example.demo.repository.TestConnectionPoolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+//    @Autowired
+//    private TestRepository testRepository;
+
     @Autowired
-    private TestRepository testRepository;
+    private TestConnectionPoolRepository testConnectionPoolRepository;
+
+
 
     @GetMapping("/test")
-    public void test() {
-        testRepository.testInsert();
+    public long test() {
+//        testRepository.testInsert();
 //        return testRepository.count();
+        return testConnectionPoolRepository.count();
     }
 }
---
+++ src/main/java/com/example/demo/repository/TestConnectionPoolRepository.java
@@ -0,0 +1,52 @@
+package com.example.demo.repository;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class TestConnectionPoolRepository {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String user;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    public long count() {
+        long id = -1L;
+        Connection con = null;
+        try {
+
+            con = BasicConnectionPool.create(url, user, password).getConnection();
+            PreparedStatement pr = con.prepareStatement("select count(*) as cc from user");
+            ResultSet resultSet = pr.executeQuery();
+
+            while (resultSet.next()) {
+                id = resultSet.getLong("cc");
+            }
+
+
+            } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                con.close();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+            return id;
+
+        }
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return id;
+    }
+}

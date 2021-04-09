c6047550884f3126880dc64d8b642c2bc3546ea9
Maksym Svystovych <none>
2021-04-09 13:07:33 +0300

    validation

---
+++ src/main/java/com/example/demo/api/UserController.java
@@ -0,0 +1,36 @@
+package com.example.demo.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+//    {
+//        "email": "string@mail.com",
+//            "id": null,
+//            "login": "string",
+//            "name": "string",
+//            "password": "string"
+//    }
+
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces =
+            MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDto> create(@Validated(UserDto.New.class) @RequestBody UserDto dto) {
+        return new ResponseEntity("service.save(dto)", HttpStatus.OK);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces =
+            MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDto> updateName(@Validated(UserDto.UpdateName.class) @RequestBody
+                                                      UserDto dto) {
+        return new ResponseEntity("service.update(dto)", HttpStatus.OK);
+    }
+}
--- src/main/java/com/example/demo/api/UserDto.java
+++ src/main/java/com/example/demo/api/UserDto.java
@@ -1,4 +1,44 @@
 package com.example.demo.api;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
+
+    interface New {
+    }
+
+    interface Exist {
+    }
+
+    interface UpdateName extends Exist {
+    }
+
+    @Null(groups = {New.class})
+    @NotNull(groups = {UpdateName.class})
+    private Long id;
+
+    @NotNull(groups = {New.class, UpdateName.class})
+    private String name;
+
+    @NotNull(groups = {New.class})
+    @Null(groups = {UpdateName.class})
+    private String login;
+
+    @NotNull(groups = {New.class})
+    @Null(groups = {UpdateName.class})
+    private String password;
+
+    @NotNull(groups = {New.class})
+    @Null(groups = {UpdateName.class})
+    @Email(groups = {New.class})
+    private String email;
 }

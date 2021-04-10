f79b5f05e7a4557bb51586689ba080622c64e17b
Maksym Svystovych <none>
2021-04-10 08:10:25 +0300

    aop

--- src/main/java/com/example/demo/aop/ExecutionTimeAspect.java
+++ src/main/java/com/example/demo/aop/ExecutionTimeAspect.java
@@ -1,5 +1,6 @@
 package com.example.demo.aop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class ExecutionTimeAspect {
 
     @Around("@annotation(LogExecutionTime)")
@@ -17,7 +19,7 @@ public class ExecutionTimeAspect {
 
         long executionTime = System.currentTimeMillis() - start;
 
-        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        log.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
         return proceed;
     }
 

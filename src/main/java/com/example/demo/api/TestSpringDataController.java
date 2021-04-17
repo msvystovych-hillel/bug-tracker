f499236806b16cab06d4f1facb219733df29aa0c
Maksym Svystovych <none>
2021-04-17 09:28:02 +0300

    spring data jpa

---
+++ src/main/java/com/example/demo/api/TestSpringDataController.java
@@ -0,0 +1,27 @@
+package com.example.demo.api;
+
+import com.example.demo.model.OldUser;
+import com.example.demo.repository.UserDataJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestSpringDataController {
+    @Autowired
+    private UserDataJpaRepository userDataJpaRepository;
+
+
+
+    @GetMapping("/old-user")
+    public OldUser testGetUser() {
+        return userDataJpaRepository.findByName("Bob");
+    }
+
+    @GetMapping("/old-user-native")
+    public OldUser testGetUserUsingNativeQuery() {
+        return userDataJpaRepository.getByNameUsingNativeQuery("Bob");
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/repository/UserDataJpaRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.OldUser;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDataJpaRepository extends CrudRepository<OldUser, Long> {
+
+    OldUser findByName(String name);
+
+    @Query("from OldUser o where o.name = :nameParam")
+    OldUser getByName(@Param("nameParam") String nameParam);
+
+    @Query(value = "select * from user o where o.name = :nameParam", nativeQuery = true)
+    OldUser getByNameUsingNativeQuery(@Param("nameParam") String nameParam);
+}

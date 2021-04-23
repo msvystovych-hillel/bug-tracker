d2527786e5fccc598903f74413bf63419fb4a9ba
Maksym Svystovych <none>
2021-04-23 15:14:20 +0300

    testing

--- src/test/java/com/example/demo/test/TestableClassTest.java
+++ src/test/java/com/example/demo/test/TestableClassTest.java
@@ -2,13 +2,23 @@ package com.example.demo.test;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class TestableClassTest {
 
+    @Mock
+    private DataService dataServiceMock;
 
     @Test
     public void test() {
@@ -24,5 +34,12 @@ class TestableClassTest {
         Mockito.when(dataServiceMock.getData()).thenReturn(data);
 
         Assertions.assertFalse(data.isEmpty());
+    }
+
+    @Test
+    public void test2() {
+        Mockito.when(dataServiceMock.getDataById(ArgumentMatchers.any()))
+                .thenReturn("dataItem");
+        Assertions.assertNotNull(dataServiceMock.getDataById("f"));
     }
 }

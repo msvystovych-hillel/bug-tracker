635c5b7f289d6268db6605e5ae88b435233f9f6d
Maksym Svystovych <none>
2021-05-01 18:14:23 +0300

    extract abstract test

---
+++ src/test/java/com/example/demo/test/AbstractIntegrationTest.java
@@ -0,0 +1,19 @@
+package com.example.demo.test;
+
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(initializers = {AbstractIntegrationTest.Initializer.class})
+public class AbstractIntegrationTest {
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + "jdbc:mysql://localhost:3306/testDB?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true",
+                    "spring.datasource.username=" + "root",
+                    "spring.datasource.password=" + "my-secret-pw"
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+}
--- src/test/java/com/example/demo/test/UserServiceIntegrationTest.java
+++ src/test/java/com/example/demo/test/UserServiceIntegrationTest.java
@@ -7,9 +7,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.util.ArrayList;
@@ -17,24 +14,12 @@ import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
-@ContextConfiguration(initializers = {UserServiceIntegrationTest.Initializer.class})
 @Slf4j
-class UserServiceIntegrationTest {
+@ContextConfiguration(initializers = {AbstractIntegrationTest.Initializer.class})
+class UserServiceIntegrationTest extends AbstractIntegrationTest {
 
     @Autowired
     private UserService userService;
-
-
-
-    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues.of(
-                    "spring.datasource.url=" + "jdbc:mysql://localhost:3306/testDB?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true",
-                    "spring.datasource.username=" + "root",
-                    "spring.datasource.password=" + "my-secret-pw"
-            ).applyTo(configurableApplicationContext.getEnvironment());
-        }
-    }
 
 
     @Test
@@ -43,7 +28,5 @@ class UserServiceIntegrationTest {
         List<User> result = new ArrayList<>();
         all.get().forEach(result::add);
         Assertions.assertFalse(result.isEmpty());
-
     }
-
 }

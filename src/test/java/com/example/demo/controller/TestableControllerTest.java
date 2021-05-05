7cfa3c7bd95124a34328cc2a49686718543fd48c
Maksym Svystovych <none>
2021-05-05 12:21:37 +0300

    add controller tests

---
+++ src/test/java/com/example/demo/controller/TestableControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = TestableController.class)
+public class TestableControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void test1() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .get("/test1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+}

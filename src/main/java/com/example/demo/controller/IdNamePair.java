9cd4cf643c8998074ecad423b3f0a4445ff68b35
Maksym Svystovych <none>
2021-05-05 12:28:44 +0300

    add controller tests

---
+++ src/main/java/com/example/demo/controller/IdNamePair.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class IdNamePair {
+    public String id;
+    public String name;
+}

2ce0f7a707be55c5c64ab3b15742f154696f5dcb
Maksym Svystovych <none>
2021-05-05 14:38:54 +0300

    add tests

--- src/main/java/com/example/demo/controller/TestableController.java
+++ src/main/java/com/example/demo/controller/TestableController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.abstractcrud.model.User;
 import com.example.demo.abstractcrud.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@Slf4j
 public class TestableController {
 
     @Autowired
@@ -31,5 +36,14 @@ public class TestableController {
     @GetMapping("/test4")
     public List<IdNamePair> test4() {
         return userService.getAllStubData();
+    }
+
+    @GetMapping("/test5")
+    public List<User> test5() {
+        Optional<Iterable<User>> all = userService.getAll();
+        List<User> result = new ArrayList<>();
+        all.get().forEach(result::add);
+        log.info("SIZE: {}", result.size());
+        return result;
     }
 }

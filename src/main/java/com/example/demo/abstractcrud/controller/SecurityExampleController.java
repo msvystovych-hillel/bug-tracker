76f14b8fe509e50373dc353e2f70d7463a1b22c4
Maksym Svystovych <none>
2021-05-09 18:34:55 +0300

    add security

--- src/main/java/com/example/demo/abstractcrud/controller/SecurityExampleController.java
+++ src/main/java/com/example/demo/abstractcrud/controller/SecurityExampleController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SecurityExampleController {
 
+    @GetMapping("/admin/get")
+    public String getAdmin() {
+        return "Hi admin";
+    }
+
     @GetMapping("/user/get")
     public String getUser() {
-        return "USER ENTITY";
+        return "Hi user";
     }
 }

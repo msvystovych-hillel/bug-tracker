f75d2878bd3857dc992e749265521f05786e92a8
Maksym Svystovych <none>
2021-05-28 08:01:55 +0300

    rabbitmq

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
-import com.example.demo.configuration.MySQLAutoconfiguration;
+import com.example.demo.producer.BroadcastConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
+    private static String ROUTING_KEY_USER_IMPORTANT_WARN = "user.important.warn";
+    private static String ROUTING_KEY_USER_IMPORTANT_ERROR = "user.important.error";
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(RabbitTemplate rabbitTemplate) {
+        String message = " payload is broadcast";
+        return args -> {
+            rabbitTemplate.convertAndSend(BroadcastConfig.FANOUT_EXCHANGE_NAME, "", "fanout" + message);
+            rabbitTemplate.convertAndSend(BroadcastConfig.TOPIC_EXCHANGE_NAME, ROUTING_KEY_USER_IMPORTANT_WARN, "topic important warn" + message);
+            rabbitTemplate.convertAndSend(BroadcastConfig.TOPIC_EXCHANGE_NAME, ROUTING_KEY_USER_IMPORTANT_ERROR, "topic important error" + message);
+        };
+    }
 }
